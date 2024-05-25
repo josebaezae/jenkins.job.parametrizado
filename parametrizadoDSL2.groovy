4bab1acdafbb89d5218ea5b348fa207d9cd6483a
josebaezae <36556587+josebaezae@users.noreply.github.com>
2024-05-25 14:07:24 -0600

    Update parametrizadoDSL2.groovy-25-05-2024

--- parametrizadoDSL2.groovy
+++ parametrizadoDSL2.groovy
@@ -1,13 +1,13 @@
 job('ejemplo3-job-DSL') {
 	description('Job DSL de ejemplo para el curso de Jenkins')
   	scm {
-      		git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
-        		node / gitConfigName('macloujulian')
-        		node / gitConfigEmail('macloujulian@gmail.com')
+      		git('https://github.com/josebaezae/jenkins.job.parametrizado.git', 'main') { node ->
+        		node / gitConfigName('josebaezae')
+        		node / gitConfigEmail('josebaezae@gmail.com')
       		}
     	} 
   	parameters {
-   		stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
+   		stringParam('nombre', defaultValue = 'Jose', description = 'Parametro de cadena para el Job Booleano')
       		choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
       		booleanParam('agente', false)
     	}
@@ -19,7 +19,7 @@ job('ejemplo3-job-DSL') {
     		shell("bash jobscript.sh")
     	}
   	publishers {
-      		mailer('macloujulian@gmail.com', true, true)
+      		mailer('josebaezae@gmail.com', true, true)
       		slackNotifier {
 		  notifyAborted(true)
 		  notifyEveryFailure(true)
